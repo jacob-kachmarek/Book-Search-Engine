5b254db4c84e4b1e931acb40ef7fc923bac38b14
Jacob Kachmarek <jacob.kachmarek@gmail.com>
2023-09-05 18:23:43 -0700

    got server working but it says jumbotron and carcolumns are no longer supported in bootstrap? Don't know how to fix

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const path = require("path");
 const db = require("./config/connection");
-const { ApolloServer } = require("apollo-server-express");
+const { ApolloServer } = require("@apollo/server")
+const { expressMiddleware } = require("@apollo/server/express4");
 const { typeDefs, resolvers } = require("./schemas");
-const { authMiddleware } = require("./utils/auth");
 // const routes = require('./routes');
 
 const app = express();
@@ -11,28 +11,33 @@ const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: authMiddleware,
 });
 
-server.applyMiddleware({ app });
+const startApolloServer = async () => {
+  await server.start();
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
-}
+  app.use('/graphql', expressMiddleware(server));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
+  // if we're in production, serve client/dist as static assets
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/dist')));
 
-db.once("open", () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
+  }
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+    });
   });
-});
+};
+
+startApolloServer();
 
 
